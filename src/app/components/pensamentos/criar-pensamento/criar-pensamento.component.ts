5bbb5344469e97f202c37e5444383fa8d1e5d127
Samuel S Rodrigues <samuelstievenrodrigues@gmail.com>
2023-05-08 16:29:17 -0300

    first commands and components

---
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent {
+
+  pensamento: Pensamento = {
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+    ) {}
+
+  criarPensamento() {
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listar-pensamento']);
+    })
+  }
+
+  cancelar() {
+    this.router.navigate(['/listar-pensamento']);
+  }
+}
---
+++ src/app/components/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { Pensamento } from '../pensamento';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent {
+  @Input() pensamento: Pensamento = {
+    id: 0,
+    conteudo: 'Olha, se você não me ama',
+    autoria: 'Manoel Gomes',
+    modelo: 'modelo3'
+  }
+
+  larguraPensamento(): string {
+    if(this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g'
+    }else {
+      return 'pensamento-p'
+    }
+  }
+}
